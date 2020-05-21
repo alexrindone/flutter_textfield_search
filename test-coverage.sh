cf97d972ec71f409aad2644aab740bb7e1c678a7
Alex Rindone <arindone@iapp.org>
2020-05-21 15:03:27 -0400

    trying with npm

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Writing Code Coverage Summary to COVERAGE.md";
 npm install lcov-parse
-node lcov-parse ./coverage/lcov.info > test-coverage.json
+node ./node_modules/lcov-parse/bin/cli.js ./coverage/lcov.info > test-coverage.json
 echo "Finished Writing to File";
