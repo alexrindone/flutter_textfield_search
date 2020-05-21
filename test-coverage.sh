0b1c9b7e4d785170fa3207f93a724504bd549f43
Alex Rindone <arindone@iapp.org>
2020-05-21 11:42:45 -0400

    another attempt

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo 'Writing Code Coverage Summary to COVERAGE.md';
-lcov --summary coverage/lcov.info | tee ./COVERAGE.md;
-ls -la
+lcov --summary coverage/lcov.info > ./COVERAGE.md
 #printf '%s\n%s\n' "# COVERAGE" "$(cat ./COVERAGE.md)" >| ./COVERAGE.md;
 cat ./COVERAGE.md;
 echo 'Finished Writing to File';
