8b1fe7d61593ffbf258315094a5f62d4c8272b4e
Alex Rindone <arindone@iapp.org>
2020-05-21 14:12:02 -0400

    testing

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Writing Code Coverage Summary to COVERAGE.md";
-lcov --summary coverage/lcov.info > ./COVERAGE.md;
+echo "test" >> ./COVERAGE.md;
+lcov --summary coverage/lcov.info >> ./COVERAGE.md;
 #echo "${MULTILINE}" > ./COVERAGE.md;
 #echo "$MULTILINE";
 echo "Test reading coverage file";
