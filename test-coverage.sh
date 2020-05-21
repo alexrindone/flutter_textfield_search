9cccec8610c50c03d245ee401bb4e41bedefa6eb
Alex Rindone <arindone@iapp.org>
2020-05-21 09:09:57 -0400

    updating test coverage script

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'Writing Code Coverage Summary to COVERAGE.md';
 lcov --summary coverage/lcov.info > ./COVERAGE.md;
-printf '%s\n%s\n' "# COVERAGE" "$(cat COVERAGE.md)" >COVERAGE.md;
+#printf '%s\n%s\n' "# COVERAGE" "$(cat COVERAGE.md)" >|COVERAGE.md;
 echo 'Finished Writing to File';
