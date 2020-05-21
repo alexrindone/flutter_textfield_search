91b9484007c3e97400063926d8535e20e60242bd
Alex Rindone <arindone@iapp.org>
2020-05-21 14:19:05 -0400

    using while loop

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Writing Code Coverage Summary to COVERAGE.md";
 echo "test" >> ./COVERAGE.md;
-lcov --summary coverage/lcov.info >> ./COVERAGE.md;
+#lcov --summary coverage/lcov.info >> ./COVERAGE.md;
+while IFS= read -r line; do
+    echo "$line" >> ./COVERAGE.md;
+done < <( lcov --summary coverage/lcov.info );
 #echo "${MULTILINE}" > ./COVERAGE.md;
 #echo "$MULTILINE";
 echo "Test reading coverage file";
