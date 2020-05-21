6e4e6a7b4874f4e0a92a1dffe69a164a7073d01e
Alex Rindone <arindone@iapp.org>
2020-05-21 10:09:36 -0400

    removing auto commit

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo 'Writing Code Coverage Summary to COVERAGE.md';
-lcov --summary coverage/lcov.info > ./COVERAGE.md;
-printf '%s\n%s\n' "# COVERAGE" "$(cat ./COVERAGE.md)" >| ./COVERAGE.md;
+lcov coverage/lcov.info -o ./COVERAGE.md;
+printf "%s\n%s\n" "# COVERAGE" "$(cat ./COVERAGE.md)" >| ./COVERAGE.md;
+cat ./COVERAGE.md;
 echo 'Finished Writing to File';
