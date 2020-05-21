abc95227ef7012adad28b7f8b14c258b9da7d6ce
Alex Rindone <arindone@iapp.org>
2020-05-21 14:54:22 -0400

    trying with npm

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 echo "Writing Code Coverage Summary to COVERAGE.md";
-echo "test" >> ./COVERAGE.md;
-#lcov --summary coverage/lcov.info >> ./COVERAGE.md;
-while IFS= read -r line; do
-    echo "$line" >> ./COVERAGE.md;
-done < <( lcov --summary coverage/lcov.info );
-#echo "${MULTILINE}" > ./COVERAGE.md;
-#echo "$MULTILINE";
-echo "Test reading coverage file";
-cat ./COVERAGE.md;
+npm install lcov-parse
+lcov-parse ./coverage/lcov.info > test-coverage.json
 echo "Finished Writing to File";
