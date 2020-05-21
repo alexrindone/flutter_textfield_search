95d32d07ad6f1c4e95e5229abb9fe7adac8e8edb
Alex Rindone <arindone@iapp.org>
2020-05-21 13:57:28 -0400

    test again

--- test-coverage.sh
+++ test-coverage.sh
@@ -2,6 +2,7 @@
 echo "Writing Code Coverage Summary to COVERAGE.md";
 MULTILINE=$(lcov --summary coverage/lcov.info);
 echo "${MULTILINE}" > ./COVERAGE.md;
+echo "$MULTILINE";
 echo "Test reading coverage file";
 cat ./COVERAGE.md;
 echo "Finished Writing to File";
